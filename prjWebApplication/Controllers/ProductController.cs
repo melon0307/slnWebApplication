edace9409d4518efd2aa5f2313e62399258aec71
Michael Lee <melon0307@hotmail.com>
2022-07-06 15:06:06 +0800

    7.6  屬性路由的設定

--- prjWebApplication/Controllers/ProductController.cs
+++ prjWebApplication/Controllers/ProductController.cs
@@ -10,38 +10,58 @@ namespace prjWebApplication.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductController : ControllerBase
+    public class ProductsController : ControllerBase
     {
         private readonly NorthwindContext _context;
 
-        public ProductController(NorthwindContext context)
+        public ProductsController(NorthwindContext context)
         {
             _context = context;
         }
 
-        public IEnumerable<Product> GetProducts()
+        //GET http://localhost:..../api/products
+        [HttpGet]
+        public IQueryable<Product> GetProducts()
         {
             return _context.Products;
         }
 
+        //GET http://localhost:..../api/products/12
         [HttpGet("{id}")]
         public Product GetProducts(int id)
         {
             return _context.Products.Find(id);
         }
 
+        //GET http://localhost:..../api/products/category/2
+
         [Route("category/{id}")]
         [HttpGet]
-        public IEnumerable<Product> GetProductsByCategoryId(int id)
+        public IQueryable<Product> GetProductsByCategoryId(int id)
         {
             return _context.Products.Where(p => p.CategoryId == id);
         }
 
         [Route("supplier/{id:int}")]
         [HttpGet]
-        public IEnumerable<Product> GetProductsBySupplierId(int id)
+        public IQueryable<Product> GetProductsBySupplerId(int id)
         {
-            return _context.Products.Where(p => p.SupplierId == id); 
+            return _context.Products.Where(p => p.SupplierId == id);
+        }
+
+        //[Route("supplier/{id:string}")]
+        //[HttpGet]
+        //public string GetProductsBySupplerId(string id)
+        //{
+        //    return id;
+        //}
+
+        //GET http://localhost:..../api/products/10/20
+        [Route("{min}/{max}")]
+        [HttpGet]
+        public IQueryable<Product> GetProductsByPrice(decimal min, decimal max)
+        {
+            return _context.Products.Where(p => p.UnitPrice >= min && p.UnitPrice <= max);
         }
     }
 }
